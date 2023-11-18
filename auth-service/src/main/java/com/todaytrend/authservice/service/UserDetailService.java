0872f0bc3150d76c41ebdd746ca0ae9b2032415b
unan25 <kunan25@naver.com>
2023-11-18 18:46:57 +0900

    feat : auth - Login시 Access, Refreh Token 발급

--- auth-service/src/main/java/com/todaytrend/authservice/service/UserDetailService.java
+++ auth-service/src/main/java/com/todaytrend/authservice/service/UserDetailService.java
@@ -3,8 +3,13 @@ package com.todaytrend.authservice.service;
 import com.todaytrend.authservice.domain.LocalUser;
 import com.todaytrend.authservice.repository.LocalUserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -12,8 +17,8 @@ public class UserDetailService implements UserDetailsService {
     private final LocalUserRepository localUserRepository;
 
     @Override
-    public LocalUser loadUserByUsername(String email) {
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         return localUserRepository.findByEmail(email)
-                .orElseThrow(() -> new IllegalArgumentException((email)));
+                .orElseThrow(() -> new UsernameNotFoundException("존재하지 않는 이메일입니다. Email : " + email));
     }
 }
