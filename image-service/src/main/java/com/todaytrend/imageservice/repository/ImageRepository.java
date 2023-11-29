43b6f1021f52e6882194ddcc9cc8ed191118d1a8
seongmin1117 <128121051+seongmin1117@users.noreply.github.com>
2023-11-29 15:53:44 +0900

    feat : 이미지 수정 , 삭제 구현

--- image-service/src/main/java/com/todaytrend/imageservice/repository/ImageRepository.java
+++ image-service/src/main/java/com/todaytrend/imageservice/repository/ImageRepository.java
@@ -6,5 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ImageRepository extends JpaRepository<Image, Long> {
+
     List<Image> findImageByPostId(Long postId);
+
+    void deleteImageByPostId(Long postId);
 }
--- image-service/src/main/java/com/todaytrend/imageservice/service/ImageService.java
+++ image-service/src/main/java/com/todaytrend/imageservice/service/ImageService.java
@@ -2,10 +2,10 @@ package com.todaytrend.imageservice.service;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.todaytrend.imageservice.dto.request.RequestImageDto;
 import com.todaytrend.imageservice.dto.response.ResponseImageDto;
 import com.todaytrend.imageservice.entity.Image;
 import com.todaytrend.imageservice.repository.ImageRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -24,23 +24,20 @@ public class ImageService {
     private final ImageRepository imageRepository;
 
     @Value("${cloud.aws.s3.bucket}")
-    private String bucket;
+    private String bucket; // S3 버킷 이름
 
     // 이미지 이름 중복 방지 UUID 붙이기
     private String changedImageName(String originName){
         String random = UUID.randomUUID().toString();
         return random+originName;
     }
-    // 이미지 업로드 Dto에서 이미지리스트만 받기
-    public List<MultipartFile> getImageList(RequestImageDto requestImageDto) {
-        return requestImageDto.getImages().stream().toList();
-    }
-
-    //MultipartFile을 받아서 S3에 저장하고 해당 파일의 URL반환
-    public String uploadImageToS3(RequestImageDto requestImageDto) throws IOException {
-        List<MultipartFile> imageList = getImageList(requestImageDto);
+    // 게시물 이미지 등록
+    public ResponseImageDto uploadImages(Long postId , MultipartFile[] images) throws IOException {
+        // 0. 반환할 이미지 dto 생성
+        ResponseImageDto imageDto = new ResponseImageDto();
+        imageDto.setPostId(postId);
         // 1. 이미지리스트에서 하나씩 S3에 저장 후 URL반환
-        for (MultipartFile image : imageList) {
+        for (MultipartFile image : images) {
             String originalFilename = image.getOriginalFilename();
             String imageName = changedImageName(originalFilename);
             // 2. S3 객체의 메타데이터 설정
@@ -48,44 +45,49 @@ public class ImageService {
             metadata.setContentLength(image.getSize());
             metadata.setContentType(image.getContentType());
             //저장할 postId 폴더가 붙은 이름 = key
-            String key =   requestImageDto.getPostId() + "/" + imageName;
+            String key =   postId + "/" + imageName;
             // 3. S3에 파일 업로드
             amazonS3.putObject(bucket, key, image.getInputStream(), metadata);
             // 4. S3에 업로드된 파일 Url을 String으로 변환
             String imageUrl = amazonS3.getUrl(bucket, key).toString();
-            // 5. Dto를 Entity로 변환, 업로드된 파일의 URL과 postId Repository에 저장
+            // 5.업로드된 파일의 URL과 postId Repository에 저장
             Image imageEntity = Image.builder()
-                    .postId(requestImageDto.getPostId())
+                    .postId(postId)
                     .imageUrl(imageUrl).build();
             imageRepository.save(imageEntity);
+            // 6. imageUrl dto에 넣어주기
+            imageDto.getImageUrlList().add(imageUrl);
         }
-        return "게시물 이미지 저장 완료";
+        return imageDto;
     }
 
-    //postId로 image 조회 후 imageUrl 리스트 반환
+    // 게시물 이미지 삭제
+    @Transactional
+    public  String deleteImages(Long postId){
+        List<Image> images = imageRepository.findImageByPostId(postId);
+        imageRepository.deleteImageByPostId(postId);
+        for (Image image : images) {
+            String key = image.getImageUrl().split(".com/")[1]; //.com/ 이후의 파일명 반환
+            amazonS3.deleteObject(bucket,key);
+        }
+    return "게시물 이미지 삭제 완료";
+    }
+    // 게시물 이미지 조회
     public ResponseImageDto findImageByPostId(Long postId) {
         List<Image> images = imageRepository.findImageByPostId(postId);
         List<String> imageUrlList = new ArrayList<>();
         for (Image image : images) {
             imageUrlList.add(image.getImageUrl());
         }
-        ResponseImageDto responseImageDto = ResponseImageDto.builder()
+        return ResponseImageDto.builder()
                 .imageUrlList(imageUrlList)
                 .postId(postId)
                 .build();
-        return responseImageDto;
     }
-
-    // multiPartFile 받아서 S3업로드 후 이미지 URL만 반환하는 로직
-    public void uploadImage(MultipartFile image) {
-        String originalName = image.getOriginalFilename();
-        String imageName = changedImageName(originalName);
-
-        ObjectMetadata metadata = new ObjectMetadata();
-        metadata.setContentLength(image.getSize());
-        metadata.setContentType(image.getContentType());
-
-        String folderName =   "user" + "/" + imageName;
-
+    // 게시물 이미지 수정
+    @Transactional
+    public ResponseImageDto updateImages(Long postId, MultipartFile[] images) throws IOException {
+        deleteImages(postId);
+        return uploadImages(postId, images);
     }
 }
