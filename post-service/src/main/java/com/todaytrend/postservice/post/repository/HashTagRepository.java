3e434f2e6fbc89a4e4ac0f282411e62ded2f8d29
jiwon0207 <linaj15@naver.com>
2023-12-09 15:40:28 +0900

    test : post- hashTag 검색 test

--- post-service/src/main/java/com/todaytrend/postservice/post/repository/HashTagRepository.java
+++ post-service/src/main/java/com/todaytrend/postservice/post/repository/HashTagRepository.java
@@ -2,13 +2,19 @@ package com.todaytrend.postservice.post.repository;
 
 import com.todaytrend.postservice.post.entity.HashTag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 @Transactional
 public interface HashTagRepository extends JpaRepository<HashTag,Long>{
 
     void deleteAllByPostId(Long postId);
+
+    @Query(value = "SELECT h.hashtag FROM HashTag h WHERE h.hashtag LIKE CONCAT('%', :keyword, '%') ")
+    List<String> findHashTagByKeyword(String keyword);
 
 }
