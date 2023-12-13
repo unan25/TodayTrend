a416baa7fe56655b5a02efdfbe87e7f2257c0a82
jiwon0207 <linaj15@naver.com>
2023-12-13 10:11:54 +0900

    fix :user feign 요청 url

--- post-service/src/main/java/com/todaytrend/postservice/post/feign/user/UserFeignClient.java
+++ post-service/src/main/java/com/todaytrend/postservice/post/feign/user/UserFeignClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "user-service", url = "localhost:8000/user/server")
+@FeignClient(name = "user-service", url = "localhost:8000/api/user/server")
 public interface UserFeignClient {
     @GetMapping("uuid/{uuid}")
     UserFeignDto findImgAndNickname(@PathVariable("uuid")String uuid);
