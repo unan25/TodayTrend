91959f5763849cc28c2a6a7d1910118bef076312
jiwon0207 <linaj15@naver.com>
2023-12-14 15:06:24 +0900

    fix: post- hahstag 검색 로직 변경

--- post-service/src/main/java/com/todaytrend/postservice/post/repository/HashTagRepository.java
+++ post-service/src/main/java/com/todaytrend/postservice/post/repository/HashTagRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.text.Normalizer;
 import java.util.List;
 
 @Repository
