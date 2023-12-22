4d673f600c1a7b572718ff336c6cc47cd19126b2
seongmin1117 <128121051+seongmin1117@users.noreply.github.com>
2023-12-22 11:50:32 +0900

    fix : 테스트 진행 후 코드 수정

--- notification-service/src/main/java/com/todaytrend/notificationservice/feignclient/ImgFeignClient.java
+++ notification-service/src/main/java/com/todaytrend/notificationservice/feignclient/ImgFeignClient.java
@@ -3,10 +3,9 @@ package com.todaytrend.notificationservice.feignclient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "IMAGE_SERVICE")
+
+@FeignClient(value = "IMAGE-SERVICE")
 public interface ImgFeignClient {
     @GetMapping("api/images/{postId}")
     ImgFeignDto getImageByPostId(@PathVariable Long postId);
