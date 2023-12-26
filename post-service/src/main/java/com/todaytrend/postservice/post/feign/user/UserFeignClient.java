071e98643fb0ba4f86f0b0075f0a4b1469877c10
unan25 <kunan25@naver.com>
2023-12-26 18:28:17 +0900

    test : 자동 CI/CD 통합 테스트 8

--- post-service/src/main/java/com/todaytrend/postservice/post/feign/user/UserFeignClient.java
+++ post-service/src/main/java/com/todaytrend/postservice/post/feign/user/UserFeignClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "user-service")
+@FeignClient(name = "user-service", url ="3.38.107.1:10000")
 public interface UserFeignClient {
     @GetMapping("api/users/server/uuid/{uuid}")
     UserFeignDto findImgAndNickname(@PathVariable("uuid")String uuid);
