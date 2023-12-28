bbf9df024b1f1555f7f8b6f2ecacc8c170b42812
unan25 <kunan25@naver.com>
2023-12-28 10:59:35 +0900

    refactor-test: 토큰 재발급 에러 처리 2

--- apigateway-server/src/main/java/com/todaytrend/apigatewayserver/config/customexceprion/CustomAuthenticationEntryPoint.java
+++ apigateway-server/src/main/java/com/todaytrend/apigatewayserver/config/customexceprion/CustomAuthenticationEntryPoint.java
@@ -1,5 +1,9 @@
 package com.todaytrend.apigatewayserver.config.customexceprion;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.server.ServerAuthenticationEntryPoint;
@@ -8,12 +12,23 @@ import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 @Component
+@Slf4j
 public class CustomAuthenticationEntryPoint implements ServerAuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper;
+
+    public CustomAuthenticationEntryPoint(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
 
     @Override
     public Mono<Void> commence(ServerWebExchange exchange, AuthenticationException authException) {
         return Mono.fromRunnable(() -> {
             exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            exchange.getResponse().getHeaders().add("Content-Type", "application/json");
+            String response = "{\"error\": \"Unauthorized\", \"message\": \"" + authException.getMessage() + "\"}";
+            exchange.getResponse().getHeaders().setContentLength(response.length());
+            exchange.getResponse().writeWith(Mono.just(exchange.getResponse().bufferFactory().wrap(response.getBytes())));
         });
     }
 }
